9e379f22fc5462a4b13fe024f4979e76d85eb5ca
David Marino <davidjmarino8@gmail.com>
2023-03-27 19:00:58 -0400

    merchants request spec complete

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -26,4 +26,23 @@ describe "Merchants API" do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+  end
 end
