19102948f1432992091df40f71cfc8d5e77a58af
David Marino <davidjmarino8@gmail.com>
2023-03-27 20:10:26 -0400

    merchant items request complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: 'merchants/items'
+      end
     end
   end
 end
