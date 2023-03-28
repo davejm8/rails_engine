40eebfdee1bc2f6fb1aae430b35db4bc5fe81998
David Marino <davidjmarino8@gmail.com>
2023-03-27 20:26:15 -0400

    add: item index and show

---
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe "Items API" do
+
+  before :each do
+    create_list(:item, 5)
+  end
+
+  it 'sends a list of all items' do
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items.count).to eq(5)
+
+    items.each do |item|
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+
+  it 'can get one item by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+  end
+end
